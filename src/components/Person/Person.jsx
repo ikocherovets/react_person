9fdcc9afbc55b1d0227d9b064e5a2bf1382ab88e
Illia Kocherovets <illiakocherovets@gmail.com>
2024-06-11 00:28:18 +0200

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const renderAge = () => {
+    if (person.age) {
+      return <p className="Person__age">I am {person.age}</p>;
+    }
+
+    return null;
+  };
+
+  const renderPartner = () => {
+    if (person.isMarried) {
+      const partnerLabel = person.sex === 'm' ? 'wife' : 'husband';
+
+      return (
+        <p className="Person__partner">
+          {person.partnerName} is my {partnerLabel}
+        </p>
+      );
+    }
+
+    return <p className="Person__partner">I am not married</p>;
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {renderAge()}
+      {renderPartner()}
+    </section>
+  );
+};
